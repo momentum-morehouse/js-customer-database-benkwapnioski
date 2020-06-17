54ab8a982e7ac13933ab41a035de6edaae91012c
Ben Kwapnioski <bkwapni@gmail.com>
2020-06-17 21:02:03 +0000

    added cell

--- main.js
+++ main.js
@@ -18,23 +18,30 @@ function listCustomers(personArray) {
     for (let customer of customers) {
         let newCustomer = document.createElement("li");
 
-        let customerText = document.createTextNode(`${customer.name.title} ${customer.name.first} ${customer.name.last} located in: ${customer.location.city} ${customer.location.state}`);
+        let customerText = document.createTextNode(`${customer.name.title} ${customer.name.first} 
+        
+        
+        ${customer.name.last} ${customer.location.city} ${customer.location.state}`);
 
         let customerEmail = document.createTextNode(`${customer.email}`);
         let customerPhone = document.createTextNode(`${customer.phone}`);
+        let customerCell = document.createTextNode(`${customer.cell}`);
 
         let address = document.createElement("p");
         let email = document.createElement("p");
         let phone = document.createElement("p");
+        let cell = document.createElement("p");
         address.classList.add("address", "measure", "lh-copy");
         email.classList.add("email", "measure", "2h-copy");
-        phone.classList.add("phone")
+        phone.classList.add("phone");
+        cell.classList.add("cell")
 
         address.append(customerText);
         email.append(customerEmail);
         phone.append(customerPhone);
+        cell.append(customerCell);
 
-        newCustomer.append(address, email, phone)
+        newCustomer.append(address, email, phone, cell)
 
         let customerImage = document.createElement("img");
         customerImage.classList.add("br-100", "h4", "w4", "dib", "ba", "b--black-05", "pa2");
