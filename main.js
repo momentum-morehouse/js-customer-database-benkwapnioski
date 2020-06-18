ae8d4d2c883768af5f768a75d29a2b029d102502
Ben Kwapnioski <bkwapni@gmail.com>
2020-06-18 15:01:20 +0000

    i need to fix the address

--- main.js
+++ main.js
@@ -12,37 +12,46 @@ let customerList = document.querySelector('#customer-list');
 console.log(customerList)
 
 function listCustomers(personArray) {
-  let customerList = document.querySelector('#customer-list');
-  console.log(customerList)
 
   for (let customer of customers) {
     let newCustomer = document.createElement("li");
     let customerText = document.createTextNode(`${customer.name.first} 
         ${customer.name.last}`);
+
     // let customerLocation = document.createTextNode(`
     // ${customer.location.street}
     // ${customer.location.city}
     // ${customer.location.state}`);
+
     let customerEmail = document.createTextNode(`${customer.email}`);
     let customerPhone = document.createTextNode(`${customer.phone}`);
     let customerCell = document.createTextNode(`${customer.cell}`);
 
+    
+    let name = document.createElement("p");
+    
     // let street = document.createElement("p");
     // let city = document.createElement("p");
     // let state = document.createElement("p");
-    let name = document.createElement("p")
+
     let email = document.createElement("p");
     let phone = document.createElement("p");
     let cell = document.createElement("p");
 
-    // location.classList.add("street", "city", "state");
+  
     name.classList.add("name");
+
+    // location.classList.add("street", "city", "state");
+
     email.classList.add("email");
     phone.classList.add("phone");
     cell.classList.add("cell")
 
-    // location.append(customerLocation);
+    
     name.append(customerText);
+
+    // location.append(customerLocation);
+
     email.append(customerEmail);
     phone.append(customerPhone);
     cell.append(customerCell);
